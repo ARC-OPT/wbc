62ca781f8b24bcfda0a43756d9b947d11d4d16ea
dmronga <dennis.mronga@dfki.de>
2019-03-20 15:16:26 +0100

    Update test case

--- test/test_wbc_velocity_scene.cpp
+++ test/test_wbc_velocity_scene.cpp
@@ -1,19 +1,12 @@
 #include <boost/test/unit_test.hpp>
-#include <kdl/chainfksolverpos_recursive.hpp>
-#include <kdl/chainjnttojacsolver.hpp>
-#include <kdl_conversions/KDLConversions.hpp>
-#include <iostream>
 #include "KinematicRobotModelKDL.hpp"
 #include "RobotModelConfig.hpp"
-#include "Jacobian.hpp"
-#include "ConstraintConfig.hpp"
 #include "WbcVelocityScene.hpp"
-#include <wbc_solvers/hls/HierarchicalLSSolver.hpp>
 
 using namespace std;
 using namespace wbc;
 
-BOOST_AUTO_TEST_CASE(test_config){
+BOOST_AUTO_TEST_CASE(test_configure){
 
     vector<string> joint_names;
     for(int i = 0; i < 7; i++)
@@ -40,64 +33,7 @@ BOOST_AUTO_TEST_CASE(test_config){
     config[0].file = std::string(getenv("AUTOPROJ_CURRENT_ROOT")) + "/control/wbc/test/data/kuka_lbr.urdf";
     BOOST_CHECK_EQUAL(robot_model->configure(config, joint_names, "kuka_lbr_base"), true);
 
-    wbc_solvers::HierarchicalLSSolver solver;
-    std::vector<int> n_constraints_pp;
-    n_constraints_pp.push_back(6);
-    solver.configure(n_constraints_pp, 7);
-
     // Configure WBC Scene
     WbcVelocityScene wbc_scene(robot_model);
     BOOST_CHECK_EQUAL(wbc_scene.configure(wbc_config), true);
-
-    // Set reference
-    CartesianState target, ref, act;
-    target.time = base::Time::now();
-    target.pose.position = base::Vector3d(0.235498, 0.822147, 1.41881);
-    target.pose.orientation.setIdentity();
-    act.pose.position.setZero();
-    act.pose.orientation.setIdentity();
-
-    // Run control loop
-    base::samples::Joints joint_state;
-    joint_state.resize(7);
-    joint_state.names = robot_model->jointNames();
-    for(int i = 0; i < 7; i++)
-        joint_state[i].position = 0.1;
-    joint_state.time = base::Time::now();
-
-    base::VectorXd solver_output;
-
-    double loop_time = 0.1; // seconds
-    while((target.pose.position - act.pose.position).norm() > 1e-4){
-
-        // Update robot model
-        BOOST_CHECK_NO_THROW(robot_model->update(joint_state));
-
-        act = robot_model->cartesianState(cart_constraint.root, cart_constraint.tip);
-        Twist diff;
-        diff = target.pose - act.pose;
-        ref.twist.linear = diff.linear;
-        ref.twist.angular = diff.angular;
-        ref.time = base::Time::now();
-        shared_ptr<CartesianVelocityConstraint> constraint = static_pointer_cast<CartesianVelocityConstraint>(wbc_scene.getConstraint("cart_pos_ctrl_left"));
-        constraint->setReference(ref);
-
-        // Compute ctrl solution
-        wbc_scene.update();
-        HierarchicalQP qp;
-        wbc_scene.getHierarchicalQP(qp);
-        solver.solve(qp, solver_output);
-
-        for(size_t i = 0; i < joint_state.size(); i++)
-            joint_state[i].position += solver_output(i) * loop_time;
-
-        cout<<"Target: x: "<<target.pose.position(0)<<" y: "<<target.pose.position(1)<<" z: "<<target.pose.position(2)<<endl;
-        cout<<"Target: qx: "<<target.pose.orientation.x()<<" qy: "<<target.pose.orientation.y()<<" qz: "<<target.pose.orientation.z()<<" qw: "<<target.pose.orientation.w()<<endl<<endl;
-
-        cout<<"Actual x: "<<act.pose.position(0)<<" y: "<<act.pose.position(1)<<" z: "<<act.pose.position(2)<<endl;
-        cout<<"Actual qx: "<<act.pose.orientation.x()<<" qy: "<<act.pose.orientation.y()<<" qz: "<<act.pose.orientation.z()<<" qw: "<<act.pose.orientation.w()<<endl;
-        cout<<"---------------------------------------------------------------------------------------------"<<endl<<endl;
-
-        usleep(loop_time * 1e6);
-    }
 }
