028a89ba533d90330990507202773067d9667fb0
dmronga <dennis.mronga@dfki.de>
2019-07-09 12:51:13 +0200

    Move PIDctrlParams to different header

---
+++ src/controllers/PIDCtrlParams.hpp
@@ -0,0 +1,24 @@
+#ifndef PID_CTRL_PARAMS_HPP
+#define PID_CTRL_PARAMS_HPP
+
+#include <base/Eigen.hpp>
+
+namespace ctrl_lib{
+
+class PIDCtrlParams{
+public:
+    PIDCtrlParams(){}
+    PIDCtrlParams(uint n){
+        p_gain.setConstant(n, 0);
+        i_gain.setConstant(n, 0);
+        d_gain.setConstant(n, 0);
+        windup.setConstant(n, std::numeric_limits<double>::max());
+    }
+    base::VectorXd p_gain;
+    base::VectorXd i_gain;
+    base::VectorXd d_gain;
+    base::VectorXd windup;
+};
+}
+
+#endif
