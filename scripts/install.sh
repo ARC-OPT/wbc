a5e932da80addad41fc6dcdbc8231a4720acdbac
Dennis Mronga <dennis.mronga@dfki.de>
2022-01-21 13:00:06 +0000

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt-get --assume-yes install git cmake build-essential libboost-system-dev libboost-program-options-dev libboost-thread-dev libboost-test-dev pkg-config libeigen3-dev 
+sudo apt-get install git cmake build-essential libboost-system-dev libboost-program-options-dev libboost-thread-dev libboost-test-dev pkg-config libeigen3-dev libboost-filesystem-dev
 
 # cmake makros
 git clone https://github.com/rock-core/base-cmake.git   
@@ -21,17 +21,20 @@ sudo make -j8 install && cd ../..
 sudo apt-get install liburdfdom-headers-dev liburdfdom-dev 
 
 # KDL 
-git clone https://github.com/orocos/orocos_kinematics_dynamics.git
+git clone https://github.com/orocos/orocos_kinematics_dynamics.git -b v1.5.1
 mkdir orocos_kinematics_dynamics/orocos_kdl/build
 cd orocos_kinematics_dynamics/orocos_kdl/build
-cmake ..
+cmake .. 
 sudo make -j8 install &&  cd ../../..
 
 # KDL parser
-sudo apt-get install libsdformat6-dev
-git clone https://github.com/rock-control/control-kdl_parser.git kdl_parser
-cd kdl_parser && mkdir build
-cd build && cmake ..
+sudo apt-get install libtinyxml2-dev
+git clone https://github.com/ros/kdl_parser.git -b 1.14.1
+cd kdl_parser/kdl_parser
+git archive --remote=git@git.hb.dfki.de:dfki-control/wbc/package_set.git HEAD patches/kdl_parser.patch | tar -x
+git apply patches/kdl_parser.patch
+mkdir build && cd build 
+cmake .. 
 sudo make -j8 install && cd ../..
 
 # If not done yet, setup a ssh key pair using the command `ssh-keygen` and add the 
