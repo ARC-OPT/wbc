57fcfbf5df8cc78d129478fd45aad8727e96ca79
ibergonzani <ibergonzani@users.noreply.github.com>
2022-08-16 09:31:00 +0000

    deploy: d2ea6263d5b58d9fff5ad7e0341b657346fcb849

--- menudata.js
+++ menudata.js
@@ -139,5 +139,4 @@ var menudata={children:[
 {text:"All",url:"globals.html"},
 {text:"Functions",url:"globals_func.html"},
 {text:"Enumerations",url:"globals_enum.html"},
-{text:"Enumerator",url:"globals_eval.html"},
-{text:"Macros",url:"globals_defs.html"}]}]}]}
+{text:"Enumerator",url:"globals_eval.html"}]}]}]}
