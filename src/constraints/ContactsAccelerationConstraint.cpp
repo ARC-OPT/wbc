79c0e5528c539343216ebf67561e3c1687afcf15
dmronga <dennis.mronga@dfki.de>
2024-05-06 12:14:46 +0200

    Distinguish between active and inactive contacts in constraints

--- src/constraints/ContactsAccelerationConstraint.cpp
+++ src/constraints/ContactsAccelerationConstraint.cpp
@@ -9,23 +9,29 @@ namespace wbc{
         uint nj = robot_model->noOfJoints();
         uint na = robot_model->noOfActuatedJoints();
         uint nc = contacts.size();
+        uint nac = contacts.getNumberOfActiveContacts();
 
         // number of optimization variables:
         // - Reduced TSID (no torques): Number robot joints nj (incl. floating base) + 6 x number of contacts nc
         // - Full TSID: Number robot joints (incl. floating base) nj + Number actuated robot joints na + 6 x number of contacts nc
         uint nv = reduced ? (nj + nc*6) : (nj + na + nc*6);
 
-        // Constrain only contact positions (number of constraints = 3 x number of contacts), not orientations (e.g. in case of point contact, the rotation is allowed to change)
-        A_mtx.resize(nc*3, nv);
-        b_vec.resize(nc*3);
+        // Constrain only contact positions (number of constraints = 3 x number of contacts), not orientations
+        // (e.g. in case of point contact, the rotation is allowed to change). Also constrain only the active contacts
+        A_mtx.resize(nac*3, nv);
+        b_vec.resize(nac*3);
 
         A_mtx.setZero();
         b_vec.setZero();
 
+        uint row_idx = 0;
         for(uint i = 0; i < contacts.size(); i++){
-            const base::Acceleration& a = robot_model->spatialAccelerationBias(robot_model->worldFrame(), contacts.names[i]);
-            b_vec.segment(i*3, 3) = -a.linear; // use only linear part of spatial acceleration bias
-            A_mtx.block(i*3,  0, 3, nj) = robot_model->spaceJacobian(robot_model->worldFrame(), contacts.names[i]).topRows<3>(); // use only top 3 rows of Jacobian (only linear part)
+            if(contacts[i].active){
+                const base::Acceleration& a = robot_model->spatialAccelerationBias(robot_model->worldFrame(), contacts.names[i]);
+                b_vec.segment(row_idx*3, 3) = -a.linear; // use only linear part of spatial acceleration bias
+                A_mtx.block(row_idx*3,  0, 3, nj) = robot_model->spaceJacobian(robot_model->worldFrame(), contacts.names[i]).topRows<3>(); // use only top 3 rows of Jacobian (only linear part)
+                row_idx++;
+            }
         }
     }
 
--- src/constraints/ContactsFrictionSurfaceConstraint.cpp
+++ src/constraints/ContactsFrictionSurfaceConstraint.cpp
@@ -9,14 +9,15 @@ void ContactsFrictionSurfaceConstraint::update(RobotModelPtr robot_model){
     uint nj = robot_model->noOfJoints();
     uint na = robot_model->noOfActuatedJoints();
     uint nc = contacts.size();
+    uint nac = contacts.getNumberOfActiveContacts();
 
     uint nv = reduced ? nj + 6*nc : nj + na + 6*nc;
 
     const uint row_skip = 16, col_skip = 6;
 
-    A_mtx.resize(nc*row_skip, nv);
-    lb_vec.resize(nc*row_skip);
-    ub_vec.resize(nc*row_skip);
+    A_mtx.resize(nac*row_skip, nv);
+    lb_vec.resize(nac*row_skip);
+    ub_vec.resize(nac*row_skip);
 
     A_mtx.setZero();
     ub_vec.setZero();
@@ -24,35 +25,40 @@ void ContactsFrictionSurfaceConstraint::update(RobotModelPtr robot_model){
 
     uint start_idx = reduced ? nj : nj + na;
 
+    uint idx = 0;
     for(uint i = 0; i < nc; i++){
-        double mu=contacts[i].mu;
-        double wx = contacts[i].wx, wy = contacts[i].wy;
 
-        Eigen::MatrixXd a(row_skip,col_skip);
-        a << -1,  0, -mu,  0,  0, 0,
-              1,  0, -mu,  0,  0, 0,
-              0, -1, -mu,  0,  0, 0,
-              0,  1, -mu,  0,  0, 0,
-              0,  0, -wy, -1,  0, 0,
-              0,  0, -wy,  1,  0, 0,
-              0,  0, -wx,  0, -1, 0,
-              0,  0, -wx,  0,  1, 0,
-              -wy, -wx, -(wx+wy)*mu,  mu,  mu, -1,
-              -wy,  wx, -(wx+wy)*mu,  mu, -mu, -1,
-               wy, -wx, -(wx+wy)*mu, -mu,  mu, -1,
-               wy,  wx, -(wx+wy)*mu, -mu, -mu, -1,
-               wy,  wx, -(wx+wy)*mu,  mu,  mu,  1,
-               wy, -wx, -(wx+wy)*mu,  mu, -mu,  1,
-              -wy,  wx, -(wx+wy)*mu, -mu,  mu,  1,
-              -wy, -wx, -(wx+wy)*mu, -mu, -mu,  1;
+        if(contacts[i].active){
+            double mu=contacts[i].mu;
+            double wx = contacts[i].wx, wy = contacts[i].wy;
 
-        Eigen::VectorXd lb(row_skip), ub(row_skip);
-        lb.setConstant(-1e10);
-        ub.setZero();
+            Eigen::MatrixXd a(row_skip,col_skip);
+            a << -1,  0, -mu,  0,  0, 0,
+                  1,  0, -mu,  0,  0, 0,
+                  0, -1, -mu,  0,  0, 0,
+                  0,  1, -mu,  0,  0, 0,
+                  0,  0, -wy, -1,  0, 0,
+                  0,  0, -wy,  1,  0, 0,
+                  0,  0, -wx,  0, -1, 0,
+                  0,  0, -wx,  0,  1, 0,
+                  -wy, -wx, -(wx+wy)*mu,  mu,  mu, -1,
+                  -wy,  wx, -(wx+wy)*mu,  mu, -mu, -1,
+                   wy, -wx, -(wx+wy)*mu, -mu,  mu, -1,
+                   wy,  wx, -(wx+wy)*mu, -mu, -mu, -1,
+                   wy,  wx, -(wx+wy)*mu,  mu,  mu,  1,
+                   wy, -wx, -(wx+wy)*mu,  mu, -mu,  1,
+                  -wy,  wx, -(wx+wy)*mu, -mu,  mu,  1,
+                  -wy, -wx, -(wx+wy)*mu, -mu, -mu,  1;
 
-        lb_vec.segment(i*row_skip,row_skip) = lb;
-        ub_vec.segment(i*row_skip,row_skip) = ub;
-        A_mtx.block<row_skip,col_skip>(i*row_skip,start_idx+i*6) = a;
+            Eigen::VectorXd lb(row_skip), ub(row_skip);
+            lb.setConstant(-1e10);
+            ub.setZero();
+
+            lb_vec.segment(idx*row_skip,row_skip) = lb;
+            ub_vec.segment(idx*row_skip,row_skip) = ub;
+            A_mtx.block<row_skip,col_skip>(idx*row_skip,start_idx+i*6) = a;
+            idx++;
+        }
     }
 }
 
