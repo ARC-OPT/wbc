da40f74427180f536528bf59d6b46dc6d45ce87e
Dennis Mronga <dmronga@DMRONGA-P14-U.dfki.uni-bremen.de>
2025-11-13 10:02:04 +0100

    Fix in Joint space tasks

--- src/tasks/JointAccelerationTask.cpp
+++ src/tasks/JointAccelerationTask.cpp
@@ -5,7 +5,7 @@ namespace wbc{
 JointAccelerationTask::JointAccelerationTask(TaskConfig config,
                                              RobotModelPtr robot_model,
                                              const std::vector<std::string>& joint_names)
-    : Task(config, robot_model, joint_names.size(), TaskType::joint_acceleration), joint_names(joint_names){
+    : Task(config, robot_model, robot_model->na(), TaskType::joint_acceleration), joint_names(joint_names){
 
 }
 
@@ -14,19 +14,16 @@ void JointAccelerationTask::update(){
     // Joint space tasks: task matrix has only ones and Zeros. The joint order in the tasks might be different than in the robot model.
     // Thus, for joint space tasks, the joint indices have to be mapped correctly.
     A.setZero();
-    for(uint k = 0; k < joint_names.size(); k++){
-        int idx = robot_model->jointIndex(joint_names[k]);
-        if(robot_model->hasFloatingBase())
-            idx += 6;
-        A(k,idx) = 1.0;
-    }
-    y_ref_world = y_ref;     // In joint space y_ref is equal to y_ref_root
-    weights_world = weights; // Same for the weights
+    y_ref_world.setZero();
+    weights_world.setZero();
+    A.block(0,robot_model->nfb(),nv,nv).setIdentity(); // Set identity for the actuated joints
+
+    y_ref_world.tail(y_ref.size()) = y_ref;     // In joint space y_ref is equal to y_ref_root
+    weights_world.tail(y_ref.size()) = weights; // Same for the weights
 }
 
 void JointAccelerationTask::setReference(const Eigen::VectorXd& ref){
     assert(ref.size() == nv);
     this->y_ref = ref;
-
 }
 } // namespace wbc
--- src/tasks/JointVelocityTask.cpp
+++ src/tasks/JointVelocityTask.cpp
@@ -14,14 +14,12 @@ void JointVelocityTask::update(){
     // Joint space tasks: task matrix has only ones and Zeros. The joint order in the tasks might be different than in the robot model.
     // Thus, for joint space tasks, the joint indices have to be mapped correctly.
     A.setZero();
-    for(uint k = 0; k < joint_names.size(); k++){
-        int idx = robot_model->jointIndex(joint_names[k]);
-        if(robot_model->hasFloatingBase())
-            idx += 6;
-        A(k,idx) = 1.0;
-    }
-    y_ref_world = y_ref;     // In joint space y_ref is equal to y_ref_root
-    weights_world = weights; // Same of the weights
+    y_ref_world.setZero();
+    weights_world.setZero();
+    A.block(0,robot_model->nfb(),nv,nv).setIdentity(); // Set identity for the actuated joints
+    
+    y_ref_world.tail(y_ref.size()) = y_ref;     // In joint space y_ref is equal to y_ref_root
+    weights_world.tail(y_ref.size()) = weights; // Same for the weights
 }
 
 void JointVelocityTask::setReference(const Eigen::VectorXd &ref){
