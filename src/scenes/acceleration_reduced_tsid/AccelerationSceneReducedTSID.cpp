f675a3b7378aa17742a86c6bf3a9535b2e1c29c8
Dennis Mronga <dmronga@DMRONGA-P14-U.Speedport_W_724V_Typ_A_05011603_06_003>
2025-11-20 13:28:46 +0100

    Mini fix

--- src/scenes/acceleration_reduced_tsid/AccelerationSceneReducedTSID.cpp
+++ src/scenes/acceleration_reduced_tsid/AccelerationSceneReducedTSID.cpp
@@ -39,7 +39,8 @@ bool AccelerationSceneReducedTSID::configure(const std::vector<TaskPtr> &tasks_i
         if(task->type != spatial_acceleration &&
            task->type != joint_acceleration &&
            task->type != com_acceleration &&
-           task->type != contact_force){
+           task->type != contact_force &&
+           task->type != contact_wrench ){
             log(logERROR)<<"Invalid task type: "<<task->type;
             return false;
         }
